c69a10c89578ca5cd43a061d9faa3d274e111eac
Elejalde Salgado Edgar David <115661362+YoDavidSalgado@users.noreply.github.com>
2025-11-25 18:57:39 -0500

    Se ajustan textos de la pagina principal en modo oscuro

--- js/appContacto.js
+++ js/appContacto.js
@@ -1,28 +1,28 @@
-const inputs = document.querySelectorAll(".contact-input");
-const toggleBtn = document.querySelector(".theme-toggle");
-const allElements = document.querySelectorAll("*");
+const inputs = document.querySelectorAll(".contact-input"); // Selecciona todos los inputs con la clase contact-input
+const toggleBtn = document.querySelector(".theme-toggle"); // Selecciona el botón para cambiar el tema
+const allElements = document.querySelectorAll("*"); // Selecciona todos los elementos del documento
 
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+toggleBtn.addEventListener("click", () => { // Añade un event listener al botón de cambio de tema
+    document.body.classList.toggle("dark"); // Cambia la clase del body para alternar entre temas
 
-    allElements.forEach((el) => {
-        el.classList.add("transition");
-        setTimeout(() => {
-            el.classList.remove("transition");
-        }, 1000);
+    allElements.forEach((el) => { // Añade una transición a todos los elementos
+        el.classList.add("transition"); // Añade la clase transition para activar la transición
+        setTimeout(() => { // Después de 1 segundo, elimina la clase transition
+            el.classList.remove("transition"); // Elimina la clase transition para finalizar la transición
+        }, 1000); // 1000 milisegundos = 1 segundo
     })
 });
 
-inputs.forEach((ipt) => {
-    ipt.addEventListener("focus", () => {
-        ipt.parentNode.classList.add("focus");
-        ipt.parentNode.classList.add("not-empty");
+inputs.forEach((ipt) => { // Recorre cada input seleccionado
+    ipt.addEventListener("focus", () => { // Añade un event listener para el evento focus
+        ipt.parentNode.classList.add("focus"); // Añade la clase focus al elemento padre del input
+        ipt.parentNode.classList.add("not-empty"); // Añade la clase not-empty al elemento padre del input
     });
 
-    ipt.addEventListener("blur", () => {
-        if (ipt.value == "") {
-            ipt.parentNode.classList.remove("not-empty");
+    ipt.addEventListener("blur", () => { // Añade un event listener para el evento blur
+        if (ipt.value == "") { // Si el valor del input está vacío
+            ipt.parentNode.classList.remove("not-empty"); // Elimina la clase not-empty del elemento padre del input
         }
-        ipt.parentNode.classList.remove("focus");
+        ipt.parentNode.classList.remove("focus"); // Elimina la clase focus del elemento padre del input
     });
 });
